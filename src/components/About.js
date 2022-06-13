e4f6c9a7c5c065a29655fb254cb5e7f475a33177
Prasun Verma <47386692+prasium@users.noreply.github.com>
2022-06-13 20:45:06 +0530

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+const About = () => {
+  return (
+    <div>
+        <h3>
+            Version 1.0.0
+        </h3>
+        <Link to="/">
+            Go back
+        </Link>
+    </div>
+  )
+}
+
+export default About;
---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
+import Button from './Button';
+
+const Header = ({title, onAdd, showAdd}) => {
+  const location = useLocation()
+  return (
+   <header className='header'>
+    <h1>
+      {title}
+    </h1>
+    {location.pathname==='/' &&(
+    <Button color={showAdd?'red':'green'} text={showAdd?'Close':'Add' }
+    onClick = {onAdd}></Button>)}
+    
+   </header>
+  )
+}
+
+// CSS in jS
+// const headingStyle = {
+//   color: 'violet', backgroundColor: 'pink'
+// };
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+export default Header
